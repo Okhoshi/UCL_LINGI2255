3f69690be713babb47c9bdc31b0d795ddb884931
Quentin Devos <uw2k7_saotome@live.be>
2013-11-19 23:16:22 +0100

    no message

--- website/models/user.py
+++ website/models/user.py
@@ -15,7 +15,8 @@ def id_path(instance, filename):
     return 'profile_pic/' + \
            (instance.first_name + instance.last_name).__hash__()
 
-class User(Entity, DUser):
+class User(Entity):
+    user = models.OneToOneField(DUser)
     confirmed_status = models.BooleanField()
     picture = models.ImageField(upload_to=pic_path)
     id_card = models.ImageField(upload_to=id_path)
