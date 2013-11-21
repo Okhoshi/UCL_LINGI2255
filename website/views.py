cfcfd9c762ea6e7e72b955c5f0d1fc198c7f4d20
Martin Crochelet <martin.crochelet@student.uclouvain.be>
2013-11-21 11:44:09 +0100

    correction

--- website/views.py
+++ website/views.py
@@ -2,8 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, \
     login as Dlogin, \
-    logout as Dlogout#, \
-    #user as DUser
+    logout as Dlogout
 from django.contrib.auth.decorators import login_required
 from forms import MForm
 from exceptions import *
