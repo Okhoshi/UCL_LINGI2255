9bd73297b714c69ae28cfe481d1d9d495c4dfbd1
RomainVw <romain.vanwelde@student.uclouvain.be>
2013-12-03 22:37:19 +0100

    evaluation

--- website/views.py
+++ website/views.py
@@ -222,7 +222,7 @@ def profile(request):
 
     rating_values = ["danger","","success"]
     feedbacks_list = []
-    for elem in feedbacks[1]:
+    for elem in feedbacks[0]:
         other = None
         req = elem.request
 
@@ -245,7 +245,7 @@ def profile(request):
 
         feedbacks_list.append(((elem.request, name_other, feedback), rating_values[rating-1]))
         
-    for elem in feedbacks[0]:
+    for elem in feedbacks[1]:
         other = None
         req = elem.request
 
