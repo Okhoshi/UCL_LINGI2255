f8d33a3db913fba32f52a529999feb7f6fd4721d
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2013-12-06 14:30:34 +0100

    Correction bug edition

--- website/views.py
+++ website/views.py
@@ -1241,7 +1241,7 @@ def modify_user(request, form):
     dusr = DUser.objects.get(username=request.user)
     dusr.username = form.user_name
     dusr.email = form.email
-    dusr.passwd = form.passwd
+    dusr.set_password(form.passwd)
     dusr.first_name = form.first_name
     dusr.last_name = form.name
 
@@ -1279,9 +1279,10 @@ def modify_organisation(request, form):
     dusr = DUser.objects.get(username=request.user)
     dusr.username = form.user_name
     dusr.email = form.email
-    dusr.passwd = form.passwd
+    dusr.set_password(form.passwd)
     dusr.first_name = form.first_name
     dusr.last_name = form.name
+
 
     # AssociationUser modify
     ausr = AssociationUser.objects.get(dj_user=dusr)
@@ -1303,7 +1304,6 @@ def modify_organisation(request, form):
                           request.FILES.get('org_pic'),
                           save=False)
 
-    print(assoc.description)
 
     # Save all in DB
     dusr.save()
@@ -1311,7 +1311,6 @@ def modify_organisation(request, form):
     ausr.entity = assoc
     ausr.save()
 
-    print(ausr.picture)
     # Relog in
     Dlogout(request)
     usr = authenticate(username=form.user_name, password=form.passwd)
